d04b694a057cf0816b874d624887e4b6b7e3a7fb
Vladislav Mikhalin <mikhalinvlad@gmail.com>
2024-12-14 15:33:06 +0300

    DmaData and Recompiler fixes (1775)
    * liverpool: fix dmadata packet handling
    
    * recompiler: emit a label right after s_branch to prevent dead code interferrence
    
    * specialize barriers

--- src/video_core/buffer_cache/buffer_cache.h
+++ src/video_core/buffer_cache/buffer_cache.h
@@ -90,6 +90,8 @@ public:
     /// Writes a value to GPU buffer.
     void InlineData(VAddr address, const void* value, u32 num_bytes, bool is_gds);
 
+    void CopyBuffer(VAddr dst, VAddr src, u32 num_bytes, bool dst_gds, bool src_gds);
+
     /// Obtains a buffer for the specified region.
     [[nodiscard]] std::pair<Buffer*, u32> ObtainBuffer(VAddr gpu_addr, u32 size, bool is_written,
                                                        bool is_texel_buffer = false,
